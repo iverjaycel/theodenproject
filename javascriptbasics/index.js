ecb9c8d7a4b9dc9b7636ebdc5242d38650b78860
iverjaycel <iverjaycel123@gmail.com>
2023-10-19 17:11:31 +0800

    update

---
+++ javascriptbasics/index.js
@@ -0,0 +1,18 @@
+
+// let name = 'iver';
+// console.log(name);
+
+// Canno  be a reserved keyword 
+// Should be meaningful
+// Connot start with  a number (1name)
+// Cannot contain a space or hyphen(-)
+// Are keys-sensiive
+// 
+
+// let firstName = 'iver';
+// let lastName =  'flako';
+
+// constant 
+const interstRate = 0.3;
+interstRate = 1; 
+console.log(interstRate); 
